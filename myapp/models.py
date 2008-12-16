16d80796afd824971f410a4c90dd6f8b788b0cf1
Waldemar Kornewald <wkornewald@localhost>
2008-12-17 00:22:18 +0100

    Sample for how to use FormWithSets, contributed by Norman Rasmussen. Really cool! Thanks!

--- myapp/models.py
+++ myapp/models.py
@@ -7,6 +7,19 @@ class Person(db.Model):
     first_name = db.StringProperty(required=True)
     last_name = db.StringProperty(required=True)
 
+    def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
     @permalink
     def get_absolute_url(self):
         return ('myapp.views.show_person', (), {'key': self.key()})
+
+class Pet(db.Model):
+    owner = db.ReferenceProperty(Person,required=True, collection_name='pet_set')
+    name = db.StringProperty(required=True)
+
+class Contract(db.Model):
+    employer = db.ReferenceProperty(Person,required=True, collection_name='employee_contract_set')
+    employee = db.ReferenceProperty(Person,required=True, collection_name='employer_contract_set')
+    start_date = db.DateTimeProperty()
+    end_date = db.DateTimeProperty()
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
+from django.forms.models import inlineformset_factory 
 from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object, delete_object, \
-    update_object
-from myapp.models import Person
+    update_object, get_model_and_form_class
+from myapp.models import Person, Contract, Pet
 from ragendja.template import render_to_response, render_to_string
+from ragendja.forms import FormWithSets
 
 def list_people(request):
     return object_list(request, Person.all(), paginate_by=10)
@@ -18,7 +20,12 @@ def add_person(request):
                                    kwargs=dict(key='%(key)s')))
 
 def edit_person(request, key):
-    return update_object(request, Person, key)
+    model, form = get_model_and_form_class(Person, None)
+    pet_formset = inlineformset_factory(model, Pet)
+    employer_formset = inlineformset_factory(model, Contract, fk_name='employee')
+    employee_formset = inlineformset_factory(model, Contract, fk_name='employer')
+    form = FormWithSets(form, (('Pets',{'formset':pet_formset}), ('Employers',{'formset':employer_formset}), ('Employees',{'formset':employee_formset})))
+    return update_object(request, model, key,  form_class=form, extra_context={'as':request.GET.get('as')})
 
 def delete_person(request, key):
     return delete_object(request, Person, object_id=key,
