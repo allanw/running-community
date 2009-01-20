68dd54ad53896802d2b49963e6ecf33cebb38ef7
Waldemar Kornewald <wkornewald@localhost>
2009-01-20 11:16:42 +0100

    Updated.

--- common/appenginepatch/ragendja/auth/urls.py
+++ common/appenginepatch/ragendja/auth/urls.py
@@ -10,13 +10,17 @@ urlpatterns = patterns('')
 LOGIN = '^%s$' % settings.LOGIN_URL.lstrip('/')
 LOGOUT = '^%s$' % settings.LOGOUT_URL.lstrip('/')
 
+# If user set a LOGOUT_REDIRECT_URL we do a redirect.
+# Otherwise we display the default template.
+LOGOUT_DATA = {'next_page': getattr(settings, 'LOGOUT_REDIRECT_URL', None)}
+
 # register auth urls depending on whether we use google or hybrid auth
 if 'ragendja.auth.middleware.GoogleAuthenticationMiddleware' in \
         settings.MIDDLEWARE_CLASSES:
     urlpatterns += patterns('',
         url(LOGIN, 'ragendja.auth.views.google_login',
             name='django.contrib.auth.views.login'),
-        url(LOGOUT, 'ragendja.auth.views.google_logout', {'next_page': '/'},
+        url(LOGOUT, 'ragendja.auth.views.google_logout', LOGOUT_DATA,
             name='django.contrib.auth.views.logout'),
     )
 elif 'ragendja.auth.middleware.HybridAuthenticationMiddleware' in \
@@ -24,11 +28,11 @@ elif 'ragendja.auth.middleware.HybridAuthenticationMiddleware' in \
     urlpatterns += patterns('',
         url(LOGIN, 'ragendja.auth.views.hybrid_login',
             name='django.contrib.auth.views.login'),
-        url(LOGOUT, 'ragendja.auth.views.hybrid_logout', {'next_page': '/'},
+        url(LOGOUT, 'ragendja.auth.views.hybrid_logout', LOGOUT_DATA,
             name='django.contrib.auth.views.logout'),
     )
 else:
     urlpatterns += patterns('',
         url(LOGIN, 'django.contrib.auth.views.login'),
-        url(LOGOUT, 'django.contrib.auth.views.logout', {'next_page': '/'}),
+        url(LOGOUT, 'django.contrib.auth.views.logout', LOGOUT_DATA,),
     )
