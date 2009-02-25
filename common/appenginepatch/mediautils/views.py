f2222bbc3407471c19d1f3193469fb8ff3c1ee93
Waldemar Kornewald <wkornewald@localhost>
2009-02-25 11:27:42 +0100

    Prepared for Django 1.1.

--- common/appenginepatch/mediautils/views.py
+++ common/appenginepatch/mediautils/views.py
@@ -8,7 +8,8 @@ from ragendja.template import render_to_response
 
 @cache_control(public=True, max_age=3600*24*60*60)
 def get_file(request, path):
-    request.user = 'hey'
+    # This is needed for proper caching
+    request.session.accessed = request.session.modified
     media_dirs = get_media_dirs()
     data = {'media_dirs': media_dirs}
     targets = get_targets(**data)
