4122423384dccd5696f06771e3135950e945fbaf
Allan Whatmough <al.whatmough@gmail.com>
2010-06-02 20:52:15 +0100

    Remove the user and user_id fields from the User model. Add a nike_user_id field

--- common/appenginepatch/ragendja/auth/hybrid_models.py
+++ common/appenginepatch/ragendja/auth/hybrid_models.py
@@ -4,12 +4,13 @@ from ragendja.auth.google_models import GoogleUserTraits
 
 class User(GoogleUserTraits):
     """User class that provides support for Django and Google Accounts."""
-    user = db.UserProperty()
-    user_id = db.StringProperty()
+    #user = db.UserProperty()
+    #user_id = db.StringProperty()
     username = db.StringProperty(required=True, verbose_name=_('username'))
     email = db.EmailProperty(verbose_name=_('e-mail address'))
     first_name = db.StringProperty(verbose_name=_('first name'))
     last_name = db.StringProperty(verbose_name=_('last name'))
+    nike_user_id = db.StringProperty(verbose_name=_('nike user id'))
 
     class Meta:
         verbose_name = _('user')
