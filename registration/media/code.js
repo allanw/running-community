97a38869007ff43118432ef4e1c000ba8436e63f
Allan Whatmough <al.whatmough@gmail.com>
2010-06-02 21:00:29 +0100

    Retrieve Nike+ id based on user supplied Nike login/password

--- registration/media/code.js
+++ registration/media/code.js
@@ -8,15 +8,16 @@ $(document).ready(function($) {
 $('#nike_account_sync').click(function() {
 	var nike_id = $('#id_nike_id').val();
 	var nike_password = $('#id_nike_password').val();
-	var url = 'http://www.allanwhatmough.com/testapp/?callback=?' + '&nike_id=' + nike_id + '&nike_password=' + nike_password; 
-	$.getJSON(url, processData);
-	function processData(data) {
+	//var url = 'http://www.allanwhatmough.com/testapp/?callback=?' + '&nike_id=' + nike_id + '&nike_password=' + nike_password; 
+        var url = '/alsapp/my_test/?nike_id=' + nike_id + '&nike_password=' + nike_password; 
+        // TODO: handle error case e.g. if it times out trying to fetch URL
+	$.getJSON(url, function(data) {
 		$('#foo').html(data);
 		data += "" // to convert it to a string
 		$('#id_nike_user_id').val(data);
-		}
-})
+	});
 
+});
 });
 
 /*$('#nike_account_sync').click(function() {
