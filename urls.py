3bf6b1f2c5219fdd285c0f683b7a2f80addbced7
Allan Whatmough <al.whatmough@gmail.com>
2012-04-26 20:15:22 +0100

    URL changes - rename alsapp to runs

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from ragendja.urlsauto import urlpatterns
 from ragendja.auth.urls import urlpatterns as auth_patterns
 from myapp.forms import UserRegistrationForm
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
@@ -11,11 +12,11 @@ handler500 = 'ragendja.views.server_error'
 
 urlpatterns = auth_patterns + patterns('',
     ('^admin/(.*)', admin.site.root),
-    (r'^$', 'django.views.generic.simple.direct_to_template',
-        {'template': 'main.html'}),
+	(r'^$', direct_to_template, {'template': 'main.html'}),
+    #url(r'^$', 'alsapp.views.index'),
     # Override the default registration form
     url(r'^account/register/$', 'registration.views.register',
         kwargs={'form_class': UserRegistrationForm},
         name='registration_register'),
-    (r'^alsapp/', include('alsapp.urls')),
+    (r'^runs/', include('runs.urls')),
 ) + urlpatterns
